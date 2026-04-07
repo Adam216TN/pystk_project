58a64842ed9ab54e8e185e6eeca0985da0cc241a
ldgom <lucas.duarte.g75@gmail.com>
2026-04-07 10:34:47 +0200

    upragde manage speed

--- src/agents/team4/speed.py
+++ src/agents/team4/speed.py
@@ -17,6 +17,20 @@ class SpeedController:
         """@private"""
         self.amax = self.c.acceleration_max
         """@private"""
+        self.azero = self.c.acceleration_czero
+        """@private"""
+        self.alow = self.c.acceleration_low 
+        """@private"""
+        self.amid = self.c.acceleration_mid 
+        """@private"""
+        self.smax = self.c.seuil_decalage_max
+        """@private"""
+        self.smid = self.c.seuil_decalage_mid
+        """"@private"""
+        self.slow = self.c.seuil_decalage_low
+        """@private"""
+        self.smin = self.c.seuil_decalage_min
+        """@private"""
     
     def reset(self) -> None:
         """Réinitialise les variables d'instances de l'agent expert"""
@@ -44,17 +58,17 @@ class SpeedController:
 
         if a < 4.0:
             return 0.98, False
-        return 1.0, False
-        if a < 6.0 :
-            return 1.0, False
+        return self.amax, False
+        if a < self.slow :
+            return self.amax, False
 
-        if a > 10.0:
-            return 0.05, True
+        if a > self.smax:
+            return self.azero, True
 
-        if (a > 5.0 and a < 8.0):
-            return 0.55, False
+        if (a > self.smin and a < self.smid):
+            return self.amid, False
 
-        if (a > 8.0 and a < 10.0):
-            return 0.3, True
+        if (a > self.smid and a < self.smax):
+            return self.alow, True
 
-        return 1.0, False
+        return self.amax, False
